bfe34ab233befea1ef417275501f2853cbbcbdb4
wintowasted <84101286+wintowasted@users.noreply.github.com>
2022-11-06 14:59:18 +0300

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-
-# MER Web Site
--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-export {default as Topbar} from './topbar/Topbar.jsx';
-export {default as Contact} from './contact/Contact.jsx';
-export {default as Intro} from './intro/Intro.jsx';
-export {default as Works} from './works/Works.jsx';
-export {default as Testimonials} from './testimonials/Testimonials.jsx';
-export {default as Portfolio} from './portfolio/Portfolio.jsx';
